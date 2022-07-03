727e96c547279a6829cba41da21a0743147c298c
Chris-Mwesigwa <96912309+Chris-Mwesigwa@users.noreply.github.com>
2022-07-03 23:00:25 +0300

    first commit
    this app does cross place connection between affordable food suppliers and customers

---
+++ apexFoodsConnect/apexFoodsConnect/src/main/java/com/apexFoodsConnect/apexFoodsConnect/ApexFoodsConnectApplication.java
@@ -0,0 +1,13 @@
+package com.apexFoodsConnect.apexFoodsConnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApexFoodsConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApexFoodsConnectApplication.class, args);
+	}
+
+}
---
+++ apexFoodsConnect/apexFoodsConnect/src/test/java/com/apexFoodsConnect/apexFoodsConnect/ApexFoodsConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apexFoodsConnect.apexFoodsConnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApexFoodsConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
